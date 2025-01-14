b5e77022d96e8c48e3768ba8d3692ebffedd2c17
Loktaru <nidalargio11@gmail.com>
2025-01-14 17:49:28 +0400

    Upload

---
+++ data.js
@@ -0,0 +1,299 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-imgl1140--",
+      "name": "IMGL1140 панорама копия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5128303334989273,
+          "pitch": 0.46017649000394556,
+          "rotation": 0,
+          "target": "2-imgl1042--"
+        },
+        {
+          "yaw": -1.4873421620937108,
+          "pitch": 0.4691623426547942,
+          "rotation": 0,
+          "target": "4-imgl1174--"
+        },
+        {
+          "yaw": 0.0008480280014833141,
+          "pitch": 0.518314833755344,
+          "rotation": 0,
+          "target": "5-imgl1211--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-imgl1002--",
+      "name": "IMGL1002 панорама копия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -3.0142787830326174,
+        "pitch": -0.04482854015519777,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.01184692968058,
+          "pitch": 0.4775763680022038,
+          "rotation": 0,
+          "target": "2-imgl1042--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-imgl1042--",
+      "name": "IMGL1042 панорама копия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.9563272387278641,
+        "pitch": 0.09494551094925541,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.882813902787725,
+          "pitch": 0.3908655870864255,
+          "rotation": 0,
+          "target": "1-imgl1002--"
+        },
+        {
+          "yaw": 0.7449034937349737,
+          "pitch": 0.46014129493812383,
+          "rotation": 0,
+          "target": "3-imgl1087-"
+        },
+        {
+          "yaw": -1.3861570823917777,
+          "pitch": 0.5301239306616736,
+          "rotation": 0,
+          "target": "0-imgl1140--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-imgl1087-",
+      "name": "IMGL1087 панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.5250838728262153,
+        "pitch": 0.13506843533767032,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3213346388887555,
+          "pitch": 0.49756699081037503,
+          "rotation": 0,
+          "target": "2-imgl1042--"
+        },
+        {
+          "yaw": -2.3171557500810085,
+          "pitch": 0.282025527737332,
+          "rotation": 0,
+          "target": "1-imgl1002--"
+        },
+        {
+          "yaw": -0.9692850008066713,
+          "pitch": -0.0559246435949472,
+          "rotation": 0,
+          "target": "0-imgl1140--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-imgl1174--",
+      "name": "IMGL1174 панорама копия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.3462970800270391,
+        "pitch": 0.3692287250739277,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8612543946366671,
+          "pitch": 0.6266632304177691,
+          "rotation": 0,
+          "target": "0-imgl1140--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-imgl1211--",
+      "name": "IMGL1211 панорама копия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.04815972277096847,
+        "pitch": 0.21158925733780976,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8946712007795243,
+          "pitch": 0.5085094035622362,
+          "rotation": 0,
+          "target": "0-imgl1140--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
