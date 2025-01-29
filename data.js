fa775a57f8fceb177bb207a7bd46c8618ddd65e7
Loktaru <nidalargio11@gmail.com>
2025-01-30 03:31:53 +0400

    Upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-hall",
-      "name": "hall",
+      "id": "0-imgl1689--",
+      "name": "IMGL1689 панорама копия",
       "levels": [
         {
           "tileSize": 256,
@@ -28,264 +28,11 @@ var APP_DATA = {
       ],
       "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": 0.06484653571210153,
-        "pitch": 0.0037480332959276552,
-        "fov": 1.3474042771833745
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 0.029801861150696496,
-          "pitch": 0.49414189371868034,
-          "rotation": 0,
-          "target": "1-bedroom"
-        },
-        {
-          "yaw": -1.4907265463832946,
-          "pitch": 0.41490650283555297,
-          "rotation": 0,
-          "target": "2-bathroom"
-        },
-        {
-          "yaw": 1.5641658348387733,
-          "pitch": 0.5286079657819016,
-          "rotation": 0,
-          "target": "4-guestroom_kitchen"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-bedroom",
-      "name": "bedroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": -0.09574509020706756,
-        "pitch": 0.21975511879266918,
-        "fov": 1.3474042771833745
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.0502376251238825,
-          "pitch": 0.7504409123697329,
-          "rotation": 0,
-          "target": "0-hall"
-        },
-        {
-          "yaw": 2.9521345948700892,
-          "pitch": 0.3221006304697873,
-          "rotation": 0,
-          "target": "4-guestroom_kitchen"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-bathroom",
-      "name": "bathroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": 0.8896990469487669,
-        "pitch": 0.1554137271232463,
-        "fov": 1.3474042771833745
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.8109479917517923,
-          "pitch": 0.31486629975212566,
-          "rotation": 0,
-          "target": "0-hall"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-guestroom-_-kitchen-2",
-      "name": "guestroom _ kitchen 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": -1.6703802266885077,
-        "pitch": 0.07797897913973806,
-        "fov": 1.3474042771833745
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.3130582694911777,
-          "pitch": 0.1650278890455219,
-          "rotation": 0,
-          "target": "5-bedroom-2"
-        },
-        {
-          "yaw": -1.3104475672238607,
-          "pitch": 0.43841891008839795,
-          "rotation": 0,
-          "target": "4-guestroom_kitchen"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-guestroom_kitchen",
-      "name": "guestroom_kitchen",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": 1.4675759552902843,
-        "pitch": 0.2463968361098967,
-        "fov": 1.3474042771833745
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.6417759074323435,
-          "pitch": 0.5055274480715077,
-          "rotation": 0,
-          "target": "3-guestroom-_-kitchen-2"
-        },
-        {
-          "yaw": 1.8850021129310397,
-          "pitch": 0.4843578587647208,
-          "rotation": 0,
-          "target": "5-bedroom-2"
-        },
-        {
-          "yaw": -1.372282008726497,
-          "pitch": 0.5681681175337889,
-          "rotation": 0,
-          "target": "0-hall"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-bedroom-2",
-      "name": "bedroom 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": 0.010780127034360376,
-        "pitch": 0.3190601869352463,
-        "fov": 1.3474042771833745
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.0185274421140322,
-          "pitch": 0.5722779284603412,
-          "rotation": 0,
-          "target": "4-guestroom_kitchen"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
@@ -293,7 +40,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
